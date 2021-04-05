a63e74393587e7f624853707e583a765b9efebc7
pinalpatel <gorasiyapinal88@gmail.com>
2021-04-04 22:32:37 -0400

    Add Parking and Display list of parking functionality added

---
+++ Database.js
@@ -0,0 +1,88 @@
+import React,{useEffect} from 'react';
+import * as SQLite from 'expo-sqlite';
+const db = SQLite.openDatabase('db.carparking');
+
+const createTable = async () => {
+  useEffect(() => {
+    db.transaction(tx => {
+      tx.executeSql('create table if not exists parking (id INTEGER primary key not null, user_id INTEGER, building_code TEXT, car_plate_no TEXT, hours_to_park TEXT, suiteno TEXT, street_address TEXT, parking_date DATE, latitude DOUBLE, longitude DOUBLE)');
+    });
+  })
+}
+
+const getData = (setData) => {
+  db.transaction(tx => {
+    tx.executeSql(
+      'select * from parking;',null,
+      (_, { rows: { _array } }) => setData(_array),
+      (_, error) => onError
+    );
+    });
+}
+
+// const getData = (setData) => {
+//   db.transaction(tx => {
+//     tx.executeSql(
+//       'select * from parking;',null,
+//       (_, result) => {
+//         console.log(result);
+//       },
+//       (_, error) => {
+//         console.log(console.error(););
+//       }
+//     );
+//     });
+// }
+
+
+const insertData = (userid,buildingCode,carPlateNo,hours,suitNoHost,parkingAddress,parkingdate,latitude,longitude) => {
+  db.transaction(
+    tx => {
+ tx.executeSql('INSERT INTO parking (user_id, building_code, car_plate_no, hours_to_park, suiteno, street_address, parking_date, latitude, longitude) VALUES (?,?,?,?,?,?,?,?,?)',[userid,buildingCode,carPlateNo,hours,suitNoHost,parkingAddress,parkingdate,latitude,longitude]);
+    },
+    null
+  );
+}
+const getDataById = (id,setItems) => {
+  db.transaction(tx => {
+    tx.executeSql(
+      'select * from parking where id = ?;', [id],
+      (_, { rows: { _array } }) => setItems(_array),
+      (_, error) => onError
+      );
+    });
+}
+
+const removeData = (id, setItems) => {
+  db.transaction(tx => {
+    tx.executeSql('delete from todoitems where id = ?;', [id],
+      (_, success) => {
+        getData(setItems)
+      },
+      (_, error) => onError
+    );
+  });
+}
+
+const updateData = (id, isCompleted, setItems) => {
+    db.transaction(tx => {
+      tx.executeSql(`update todoitems set isCompleted = ? where id = ?;`, [isCompleted == 0 ? 1 : 0, id],
+      (_, success) => {
+        getData(setItems)
+      },
+      (_, error) => onError);
+    })
+}
+
+const onError = (_, error) => {
+    console.log(error);
+}
+
+export const Database = {
+    createTable,
+    insertData,
+    updateData,
+    getData,
+    getDataById,
+    removeData
+}
---
+++ ParkingListScreen.js
@@ -0,0 +1,112 @@
+import React,{ useState, useEffect } from 'react';
+import {StyleSheet, View, Text, FlatList,Image, ActivityIndicator, Pressable} from 'react-native';
+import {Database} from './Database'
+
+function ParkingListScreen({navigation, route}){
+
+  const [data, setData] = useState(null);
+  const [isLoading, setLoading] = useState(true);
+
+   useEffect(() => {
+  Database.getData(setData);
+ }, []);
+
+  return(
+    <View style={styles.container}>
+      <FlatList
+        data = {data}
+        keyExtractor = { (item, index) => {return item.car_plate_no;} }
+        renderItem = { ({item}) => (
+          <Pressable onPress={() => {navigation.navigate('ParkingDetailScreen', { id: item.id,});}}>
+
+            <View>
+              <View style={styles.listitem}>
+              <View style={styles.view}>
+              <Image style={styles.image} source={require('./assets/location.png')} />
+              <Text style={styles.address}> {item.street_address}</Text>
+
+              </View>
+                <Text style={styles.hourspark}> Parking Hours:  Maximum {item.hours_to_park} </Text>
+                <Text style={styles.hourspark}> Parking Date:  fix </Text>
+                <View style={styles.carPlateView} >
+                  <Text style={styles.carPlate}>Car Plate No :  {item.car_plate_no} </Text>
+                </View>
+              </View>
+            </View>
+          </Pressable>
+        )
+
+      }/>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+  },
+  thumb: {
+    width: '90%',
+    height: 150,
+    padding: 10,
+    borderRadius: 10,
+  },
+  listitem: {
+    flexDirection: 'column',
+    borderRadius:4,
+    backgroundColor: 'white',
+    width: '95%',
+    marginLeft:10,
+    marginRight:10,
+    marginBottom:10,
+    marginTop:5,
+    borderColor:'#798AFF',
+    borderWidth: 1,
+
+  },
+  title: {
+    fontSize: 20,
+    color: '#123456',
+    color:'white',
+  },
+  hourspark: {
+    fontSize: 15,
+    padding: 10,
+    color:'black',
+  },
+  address: {
+    fontSize: 15,
+    textAlign: 'center',
+    padding: 10,
+    color:'black',
+    fontWeight: 'bold',
+  },
+  carPlateView:{
+    backgroundColor: '#798AFF',
+    borderBottomLeftRadius:4,
+    borderBottomRightRadius:4,
+    padding: 6,
+    alignItems:'center',
+},
+image:{
+  height:28,
+  width:21,
+  marginLeft:10,
+  tintColor:'black',
+},
+view:{
+  height:50,
+  borderRadius: 4,
+  width: '100%',
+  flexDirection: 'row',
+  alignItems: 'center',
+},
+carPlate:{
+fontSize: 15,
+color:'white',
+},
+
+});
+
+export default ParkingListScreen;
